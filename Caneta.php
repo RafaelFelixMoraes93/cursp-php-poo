11d00bde49b81a2d0a89a0eb4026faf57a0bd6e5
Rafael <rafael.felix.moraes@gmail.com>
2025-04-06 17:46:00 -0300

    Primeiro commit

---
+++ Caneta.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of Caneta
+ *
+ * @author favin
+ */
+class Caneta {
+    //put your code here
+}
