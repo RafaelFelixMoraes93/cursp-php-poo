3723466febe51605e62f7802ad3b6fff18d66e1a
Rafael <rafael.felix.moraes@gmail.com>
2025-04-06 18:56:18 -0300

    Feat Adicionando Classe Caneta

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Aula 02 - POO</title>
     </head>
     <body>
+        <pre>
         <?php
-        // put your code here
+            require_once 'Caneta.php';
+            $c1 = new Caneta();
+            $c1->modelo = "Bic";
+            $c1->cor = "Azul";
+            $c1->ponta = 0.5;
+            $c1->carga = 50;
+            $c1->tampada = false;  
+            print_r($c1);
+            $c1->destampar();
+            $c1->rabiscar();
+            
+            $c2 = new Caneta();
+            $c2->modelo = "Bic";
+            $c2->cor = "Verde";
+            $c2->ponta = 0.5;
+            $c2->carga = 50;
+            $c2->tampar();
+            print_r($c2);
+            $c2->rabiscar();
         ?>
+        </pre>
     </body>
 </html>
