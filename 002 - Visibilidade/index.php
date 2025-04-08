e6a2a344aa1cd97aa04416c11a39343f4147cf50
Rafael <rafael.felix.moraes@gmail.com>
2025-04-07 21:03:32 -0300

    Feat Adicionando Visibilidade

---
+++ 002 - Visibilidade/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02 - POO</title>
+    </head>
+    <body>
+        <pre>
+        <?php
+            require_once 'Caneta.php';
+            $c1 = new Caneta();
+            $c1->modelo = "Bic Cristal";
+            $c1->cor = "Azul";
+            //$c1->ponta = 0.5;
+            //$c1->carga = 99;
+            //$c1->tampada = true;
+            print_r($c1);
+            $c1->rabiscar();
+            $c1->tampar();
+            print_r($c1);
+            $c1->rabiscar();
+        ?>
+        </pre>
+    </body>
+</html>
