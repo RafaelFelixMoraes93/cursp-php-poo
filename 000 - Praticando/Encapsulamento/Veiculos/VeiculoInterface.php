7be150efd970fcbe4b9a3d931c1bac01db502ca0
Rafael <rafael.felix.moraes@gmail.com>
2025-04-12 20:12:00 -0300

    Feat Adicionado VeiculoInterface

---
+++ 000 - Praticando/Encapsulamento/Veiculos/VeiculoInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+interface VeiculoInterface {
+public function aceletar();
+public function frear();
+public function ligar();
+public function desligar();
+public function exibirFicha();
+}
