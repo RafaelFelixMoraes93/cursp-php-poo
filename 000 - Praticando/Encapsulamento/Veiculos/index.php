9aff70d651621016de636132b37c25290e797a6b
Rafael <rafael.felix.moraes@gmail.com>
2025-04-12 20:11:07 -0300

    Feat Adicionando index

--- 000 - Praticando/Encapsulamento/Veiculos/index.php
+++ 000 - Praticando/Encapsulamento/Veiculos/index.php
@@ -6,8 +6,24 @@
     </head>
     <body>
         <pre>
-        <?php            
-                        
+        <?php
+            require_once 'Carro.php';
+            $carro = new Carro("Fusion", "2010", "Preto", "4584955 ");
+            echo '<h1>Exemplo de uma carro</h1>';           
+            $carro->exibirFicha();
+            
+            echo '<p>Tentando acelerar carro desligado</p>';
+            $carro->aceletar();
+            $carro->exibirFicha();
+            
+            echo '<p>Ligando carro e acelerando</p>';
+            $carro->ligar();
+            $carro->aceletar();
+            $carro->exibirFicha();
+            
+            echo '<p>Tentando desligar carro em movimento</p>';
+            $carro->desligar();
+            $carro->exibirFicha();
         ?>
         </pre>
     </body>
