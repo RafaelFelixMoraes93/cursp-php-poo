ea70c9b95be5cc9e6e83010112ae688291ad0918
Rafael <rafael.felix.moraes@gmail.com>
2025-04-13 00:31:13 -0300

    Feat Adicionado Interface Luta

---
+++ 005 - Objetos Compostos/LutaInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+interface LutaInterface {
+public function apresentar();
+public function status();
+public function ganharLuta();
+public function perderLuta();
+public function empatarLuta();
+}
