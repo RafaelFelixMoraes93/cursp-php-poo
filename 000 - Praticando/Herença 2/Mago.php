40f7529c3c60770f309b9430b3726d98531bd3f1
Rafael <rafael.felix.moraes@gmail.com>
2025-04-16 18:45:38 -0300

    Feat Classe Mago

---
+++ 000 - Praticando/Herença 2/Mago.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'Ser.php';
+require_once 'SerInterface.php';
+class Mago extends Ser implements SerInterface{    
+    private $boladefogo;
+    public function __construct($nome, $vida, $mana) {
+        $this->setNome($nome);
+        $this->setVida($vida);
+        $this->setMana($mana);
+        $this->setVocacao("Mago");
+    }
+    
+    public function getVocacao() {
+        return $this->vocacao;
+    }
+
+    public function setVocacao($vocacao): void {
+        $this->vocacao = $vocacao;
+    }
+    
+    public function getBoladefogo() {
+        return $this->boladefogo;
+    }
+
+    public function setBoladefogo($boladefogo): void {
+        $this->boladefogo = $boladefogo;
+    }
+    
+    
+    #[\Override]
+    public function exebirFicha() {
+        echo "<br>";
+        echo "Nome: {$this->getNome()}<br>Vocação: {$this->getVocacao()}<br>Vida: {$this->getVida()}<br>Mana: {$this->getMana()}";
+        echo "<br>";
+    }
+}
