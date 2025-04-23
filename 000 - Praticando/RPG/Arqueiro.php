39c2764be04f4d3d90d5caea9358e7745784cf93
Rafael <rafael.felix.moraes@gmail.com>
2025-04-23 20:32:40 -0300

    Feat Classe Arqueiro

---
+++ 000 - Praticando/RPG/Arqueiro.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Humano.php';
+require_once 'FuncoesInterface.php';
+class Arqueiro extends Humano implements FuncoesInterface{
+    public function __construct($nome, $sexo) {
+        $this->nome = $nome;
+        $this->sexo = $sexo;
+        $this->vida = 75;
+        $this->mana = 75;
+    }
+    
+    #[\Override]
+    public function curarVida($pocao) {
+        $this->vida = $this->getVida() + $pocao;
+    }
+
+    #[\Override]
+    public function encherMana($pocao) {
+        $this->mana = $this->getMana() + $pocao;
+    }
+
+    #[\Override]
+    public function exibirFicha() {
+        echo "Nome: {$this->nome}<br>";
+        echo "Sexo: {$this->sexo}<br>";
+        echo "Vida: {$this->vida}<br>";
+        echo "Mana: {$this->mana}<br>";
+    }
+}
