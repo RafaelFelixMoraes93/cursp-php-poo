77202a5adb7930431dad715a578488d9e01fd9ae
Rafael <rafael.felix.moraes@gmail.com>
2025-04-27 00:22:47 -0300

    Refactor index

--- 000 - Praticando/RPG/index.php
+++ 000 - Praticando/RPG/index.php
@@ -6,19 +6,20 @@
         <title>RPG</title>
     </head>
     <body>
+        <pre>
         <?php
             require_once 'Humano.php';
             require_once 'Mago.php';
             require_once 'Guerreiro.php';
             require_once 'Arqueiro.php';
             require_once 'FuncoesInterface.php';
-            require_once 'Espada.php';
-            require_once 'Elmo.php';
-            require_once 'Armaduras.php';
-            require_once 'Escudo.php';
-            require_once 'Pernas.php';
-            require_once 'HealthPotion.php';
-            require_once 'ManaPotion.php';
+            require_once 'Armas/Espada.php';
+            require_once 'Elmo/Elmo.php';
+            require_once 'Armaduras/Armadura.php';
+            require_once 'Escudos/Escudo.php';
+            require_once 'Pernas/Pernas.php';
+            require_once 'ItensDeCura/HealthPotion.php';
+            require_once 'ItensDeCura/ManaPotion.php';
             
             // Espadas
             $knife = new Espada("Knife", 5);
@@ -41,8 +42,14 @@
             // Elmos
             $elmoDeMadeira = new Elmo("Elmo de Madeira", 2);
             
+            echo "<hr>";
+            echo "<h1>RPG</h1>";
+            echo "<hr>";
             
+            $mago = new Mago("Draxido", "M");
+            $mago->exibirFicha();
             
         ?>
+        </pre>
     </body>
 </html>
