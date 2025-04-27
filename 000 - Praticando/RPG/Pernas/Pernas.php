c911d8226b4d51a8eba488b06908b838edb1ac4e
Rafael <rafael.felix.moraes@gmail.com>
2025-04-27 00:24:20 -0300

    Feat Classe Pernas

---
+++ 000 - Praticando/RPG/Pernas/Pernas.php
@@ -0,0 +1,29 @@
+<?php
+
+class Pernas {
+    private $nome;
+    private $defesa;
+    
+    public function __construct($nome, $defesa) {
+        $this->nome = $nome;
+        $this->defesa = $defesa;
+    }
+
+    
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getDefesa() {
+        return $this->defesa;
+    }
+
+    public function setNome($nome): void {
+        $this->nome = $nome;
+    }
+
+    public function setDefesa($defesa): void {
+        $this->defesa = $defesa;
+    }
+
+}
